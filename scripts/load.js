e62393b715ea81c225412bb51cb2313df763e4c6
jaspior <38392025+jaspior@users.noreply.github.com>
2020-06-29 02:46:38 -0300

    Add files via upload

---
+++ scripts/load.js
@@ -0,0 +1,16 @@
+function preload() {
+
+  imagemPersonagem = loadImage('imagens/personagem/ratolino.png');
+  
+//n consegui fazer um for, paciência  
+  
+ imagemCenario1.push(loadImage('imagens/cenarios/morro/Hills_1.png'));
+  imagemCenario1.push(loadImage('imagens/cenarios/morro/Hills_2.png'));
+  imagemCenario1.push(loadImage('imagens/cenarios/morro/Hills_3.png'));
+  imagemCenario1.push(loadImage('imagens/cenarios/morro/Hills_4.png'));
+  imagemCenario1.push(loadImage('imagens/cenarios/morro/Hills_5.png'));
+  imagemCenario1.push(loadImage('imagens/cenarios/morro/Hills_6.png'));
+       
+  
+
+}
---
+++ scripts/var.js
@@ -0,0 +1,16 @@
+let imagemPersonagem;
+var imagemCenario1 = new Array();
+var cenario1 = [];
+
+
+
+const matrizPersonagem = [
+  [0,0],
+  [0,100],
+  [0,200],
+  [100,0],
+  [100,100],
+  [100,200],
+  [200,0],
+  [200,100],
+];
